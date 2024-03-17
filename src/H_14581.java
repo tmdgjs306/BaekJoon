71aa049989b200b311d9a6ce81637be7aa7cca17
tmdgjs306 <58868717+tmdgjs306@users.noreply.github.com>
2024-03-17 20:26:56 +0900

    알고리즘스터디(구현)_B5

---
+++ src/H_14581.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class H_14581 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String [][] strings = new String[3][3];
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                strings[i][j] = ":fan:";
+            }
+        }
+        strings[1][1] = ":"+s+":";
+        for(int i=0; i<3; i++){
+            for (int j=0; j<3; j++){
+                System.out.print(strings[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
