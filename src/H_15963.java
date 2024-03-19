df1fd0531538e88cd26aacffa5833ca52bd1d9b3
tmdgjs306 <58868717+tmdgjs306@users.noreply.github.com>
2024-03-19 20:14:05 +0900

    알고리즘스터디(구현)_B5

---
+++ src/H_15963.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class H_15963 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long m = sc.nextLong();
+        if(n==m){
+            System.out.println(1);
+        }
+        else {
+            System.out.println(0);
+        }
+    }
+}
