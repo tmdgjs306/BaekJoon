f37f46ad99f843b723664b55182a6a59371e6a1d
tmdgjs306 <58868717+tmdgjs306@users.noreply.github.com>
2024-04-14 21:00:55 +0900

    알고리즘스터디(구현|문자열)_B3

--- src/H_11718.java
+++ src/H_11718.java
@@ -3,13 +3,11 @@ import java.util.Scanner;
 public class H_11718 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int c = sc.nextInt();
-            int min = Math.min(a,Math.min(b,c));
-            int d = sc.nextInt();
-            int e = sc.nextInt();
-            int min2 = Math.min(d,e);
-        System.out.println((min+min2)-50);
+        StringBuilder sb = new StringBuilder();
+        while (sc.hasNextLine()){
+            String s = sc.nextLine();
+            sb.append(s).append("\n");
+        }
+        System.out.println(sb);
     }
 }
